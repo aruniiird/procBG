00dff30e60fcb20718013ea5df236720eebcda0b
Arun Kumar Mohan <arun.iiird@gmail.com>
2018-12-28 08:16:35 +0530

    Added command execution and verbose mode
    This will start the command provided as argument and returns the pid.
    
    Signed-off-by: Arun Kumar Mohan <arun.iiird@gmail.com>

--- src/toBG/toBG.go
+++ src/toBG/toBG.go
@@ -6,6 +6,14 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+)
+
+const (
+	// ErrorLabel prefix label for error messages
+	ErrorLabel = "[ ERROR ] "
+	// VerboseLogFlags log flags for verbose mode
+	VerboseLogFlags = log.LstdFlags | log.Lshortfile
 )
 
 var (
@@ -15,7 +23,8 @@ var (
 	stdErr   io.Writer
 	logO     *log.Logger
 	logE     *log.Logger
-	logFlags = log.LstdFlags | log.Lshortfile
+	logFlags = 0
+	verbose  bool
 )
 
 func setOutErrFiles(outF, errF string) (stdOut, stdErr io.Writer, err error) {
@@ -43,29 +52,52 @@ func setOutErrFiles(outF, errF string) (stdOut, stdErr io.Writer, err error) {
 
 func initLog() {
 	logO = log.New(os.Stdout, "", logFlags)
-	logE = log.New(os.Stderr, "", logFlags)
+	logE = log.New(os.Stderr, ErrorLabel, VerboseLogFlags)
 }
 
 func initLogWithFiles(stdO, stdE io.Writer) {
+	if !verbose {
+		return
+	}
 	outW := io.MultiWriter(os.Stdout, stdO)
 	errW := io.MultiWriter(os.Stderr, stdE)
 	logO = log.New(outW, "", logFlags)
-	logE = log.New(errW, "", logFlags)
+	logE = log.New(errW, ErrorLabel, VerboseLogFlags)
 }
 
 func init() {
 	flag.StringVar(&outF, "out", "", "Command's output file")
 	flag.StringVar(&errF, "err", "", "Command's error output file")
+	flag.BoolVar(&verbose, "verbose", false, "Verbose mode")
 	initLog()
 }
 
 func main() {
 	flag.Parse()
 	stdOut, stdErr, err := setOutErrFiles(outF, errF)
+	if verbose {
+		logFlags = VerboseLogFlags
+	}
 	if err != nil {
 		logE.Fatalln("Error:", err)
 	}
 	initLogWithFiles(stdOut, stdErr)
-	logO.Println("Printed to stdout")
-	logE.Println("Printed to stderr")
+	mainCmdStr := flag.Args()[0]
+	otherArgs := flag.Args()[1:]
+	if mCmd, err := exec.LookPath(mainCmdStr); err == nil {
+		mainCmdStr = mCmd
+	}
+	mainCmd := exec.Command(mainCmdStr, otherArgs...)
+	mainCmd.Stdout = stdOut
+	mainCmd.Stderr = stdErr
+	if verbose {
+		logO.Println("Main Command    : ", mainCmdStr)
+		logO.Println("Other Arguments : ", otherArgs)
+		logO.Println("Arguments Length: ", len(otherArgs))
+	}
+	if err := mainCmd.Start(); err != nil {
+		logE.Println("Failed to start the command: ", err)
+		os.Exit(1)
+	}
+	logO.Println(mainCmd.Process.Pid)
 }
